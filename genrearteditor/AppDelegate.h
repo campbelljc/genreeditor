f8f6110e2c8ecc651267b2c8ba1745c8f64a689f
JCC <jothesmo@gmail.com>
2015-08-20 16:44:27 -0400

    Initial Commit

---
+++ genrearteditor/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  genrearteditor
+//
+//  Created by Jonathan Campbell on 2015-08-20.
+//  Copyright (c) 2015 Jonathan Campbell. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ genrearteditor/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  genrearteditor
+//
+//  Created by Jonathan Campbell on 2015-08-20.
+//  Copyright (c) 2015 Jonathan Campbell. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
